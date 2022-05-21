d0eecc8b94a17e5d7dadde8d85b5bbd5f82ace68
kou47 <ottuntun47@gmail.com>
2022-05-21 21:00:37 +0900

    Initial commit

---
+++ app/src/main/java/com/websarva/wings/android/myapplication3/MyAdapter.java
@@ -0,0 +1,45 @@
+package com.websarva.wings.android.myapplication3;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SimpleAdapter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// カスタムアダプター
+public class MyAdapter extends SimpleAdapter {
+
+    // 外部から呼び出し可能なマップ
+    public Map<Integer,Boolean> checkList = new HashMap<>();
+
+    public MyAdapter(Context context, List<? extends Map<String, ?>> data,
+                     int resource, String[] from, int[] to) {
+        super(context, data, resource, from, to);
+
+        // 初期値を設定する
+        for(int i=0; i<data.size();i++){
+            Map map = (Map)data.get(i);
+            checkList.put(i,(Boolean)map.get("check"));
+        }
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = super.getView(position, convertView, parent);
+        CheckBox ch = view.findViewById(R.id.checkBox);
+
+        // チェックの状態が変化した場合はマップに記憶する
+        ch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                checkList.put(position,isChecked);
+            }
+        });
+        return view;
+    }
+}
---
+++ app/src/main/java/com/websarva/wings/android/myapplication3/ui/home/HomeFragment.java
@@ -0,0 +1,36 @@
+package com.websarva.wings.android.myapplication3.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.websarva.wings.android.myapplication3.R;
+import com.websarva.wings.android.myapplication3.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
+
+    public void onViewCreated(){
+
+    }
+}
