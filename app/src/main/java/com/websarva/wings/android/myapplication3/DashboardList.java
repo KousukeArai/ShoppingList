90f9eeef15daf5c8529366e10fefc764bb584983
Soybanana <yutty0613@au.com>
2022-05-23 16:50:11 +0900

    DashboardList.javaファイルの追加再push

---
+++ app/src/main/java/com/websarva/wings/android/myapplication3/DashboardList.java
@@ -0,0 +1,34 @@
+package com.websarva.wings.android.myapplication3;
+
+import android.database.Cursor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DashboardList {
+    public static List makeList (Cursor cursor) {
+
+        List _buyList =MainActivity.createList();
+
+
+        //データベースから取得した値を格納する変数の用意。データがなかった時のための初期値も用意
+        String name;
+        String unit;
+        //SQL実行の戻り値であるカーソルオブジェクトをループさせてデータベース内のデータを取得
+        while (cursor.moveToNext()) {
+            Map<String, String> map = new HashMap();
+            //カラムのインデックス値を取得
+            int idxName = cursor.getColumnIndex("name");
+            name = cursor.getString(idxName);
+            map.put("name", name);
+
+            int idxUnit = cursor.getColumnIndex("unit");
+            unit = cursor.getString(idxUnit);
+            map.put("unit", unit);
+
+            _buyList.add(map);
+        }
+        return _buyList;
+    }
+}
